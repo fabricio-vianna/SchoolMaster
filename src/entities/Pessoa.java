fd8c016739124c1039c250fef829407fb58b7bc7
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-09-15 14:41:16 -0300

    feat: adicionar classe abstrata Pessoa com atributos básicos

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,58 @@
+package entities;
+
+public abstract class Pessoa {
+
+    private Integer id;
+    private String nome;
+    private Integer cpf;
+    private String email;
+
+    public Pessoa(Integer id, String nome, Integer cpf, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
