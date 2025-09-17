92cc2db028ebb2e9ad3b0821ddef019057bdb58a
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-09-17 12:12:51 -0300

    "feat: adicionar método para adicionar professor ao curso"

--- src/services/CursoService.java
+++ src/services/CursoService.java
@@ -11,7 +11,12 @@ import entities.Professor;
 
 public class CursoService {
 
-    public static Disciplina criarDisciplinaNoCurso(Integer id, String nome, Integer cargaHoraria, Professor professor, Curso curso) {
+    private static List<Curso> cursosCriados = new ArrayList<>();
+
+    private CursoService() {
+    }
+
+    public static Disciplina adicionarDisciplinaAoCurso(Integer id, String nome, Integer cargaHoraria, Professor professor, Curso curso) {
         if (professor == null || curso == null) {
             throw new IllegalArgumentException("Curso e Professor não podem ser nulos.");
         }
@@ -22,27 +27,34 @@ public class CursoService {
         return d;
     }
 
-    private static List<Curso> cursosCriados = new ArrayList<>();
+    public static boolean existe(Integer id, String nome) {
+        return cursosCriados.stream()
+                .anyMatch(c -> c.getId().equals(id) || c.getNome().equalsIgnoreCase(nome));
+    }
 
     public static Curso criarCurso(Integer id, String nome) {
-        boolean existe = cursosCriados.stream()
-                .anyMatch(c -> c.getId().equals(id) || c.getNome().equalsIgnoreCase(nome));
-
-        if (existe) {
+        if (existe(id, nome)) {
             throw new IllegalArgumentException("Curso com esse ID ou nome já existe!");
         }
         Curso curso = new Curso(id, nome);
         cursosCriados.add(curso);
-
         return curso;
     }
 
-    public static Aluno criarAluno(Integer id, String nome, String cpf, String email, String matricula, Curso curso) {
+    public static Aluno adicionarAlunoAoCurso(Integer id, String nome, String cpf, String email, String matricula, Curso curso) {
         Aluno aluno = new Aluno(id, nome, cpf, email, matricula, curso);
         if (!curso.getListaAlunos().contains(aluno)) {
             curso.adicionarAluno(aluno);
         }
         return aluno;
+    }
+
+    public static Professor adicionarProfessorAoCurso(Integer id, String nome, String cpf, String email, String especialidade, Curso curso) {
+        Professor professor = new Professor(id, nome, cpf, email, especialidade, curso);
+        if (!curso.getListaProfessor().contains(professor)) {
+            curso.adicionarProfessor(professor);
+        }
+        return professor;
     }
 
     public static Avaliacao registrarAvaliacao(Integer id, Aluno aluno, Disciplina disciplina, Double nota, Integer frequencia) {
