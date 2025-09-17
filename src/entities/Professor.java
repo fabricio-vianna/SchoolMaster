efa7bbe88f0cb76421031d84730ab78da8606b87
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-09-17 12:39:14 -0300

    "feat: adicionar atributo curso ao professor"

--- src/entities/Professor.java
+++ src/entities/Professor.java
@@ -8,13 +8,16 @@ public class Professor extends Pessoa {
     private String especialidade;
     private List<Disciplina> listaDisciplinas = new ArrayList<>();
 
+    private Curso curso;
+
     public Professor(Integer id, String nome, String cpf, String email) {
         super(id, nome, cpf, email);
     }
 
-    public Professor(Integer id, String nome, String cpf, String email, String especialidade) {
+    public Professor(Integer id, String nome, String cpf, String email, String especialidade, Curso curso) {
         super(id, nome, cpf, email);
         this.especialidade = especialidade;
+        this.curso = curso;
     }
 
     public String getEspecialidade() {
@@ -36,4 +39,10 @@ public class Professor extends Pessoa {
     public List<Disciplina> listarDisciplinas() {
         return listaDisciplinas;
     }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+
 }
