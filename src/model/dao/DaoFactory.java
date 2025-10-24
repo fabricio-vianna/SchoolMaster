329511ba76fd6e5e6527c1123940cacb7fd7a082
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-10-24 17:22:40 -0300

    feat: adicionar DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,32 @@
+package model.dao;
+
+import db.DB;
+import model.entities.Disciplina;
+import model.entities.Professor;
+
+public class DaoFactory {
+
+    public static AlunoDao createAlunoDao() {
+        return new AlunoDaoJDBC(DB.getConnection());
+    }
+
+    public static AvaliacaoDao createAvaliacaoDao() {
+        return new AvaliacaoDaoJDBC(DB.getConnection());
+    }
+
+    public static CursoDao createCursoDao() {
+        return new CursoDaoJDBC(DB.getConnection());
+    }
+
+    public static Disciplina createDisciplinaDao() {
+        return new DisciplinaDaoJDBC(DB.getConnection());
+    }
+
+    public static MatriculaDao createMatriculaDao() {
+        return new MatriculaDaoJDBC(DB.getConnection());
+    }
+
+    public static Professor createProfessorDao() {
+        return new ProfessorDaoJDBC(DB.getConnection());
+    }
+}
