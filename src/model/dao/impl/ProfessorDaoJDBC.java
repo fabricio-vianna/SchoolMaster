0e0c6b27e267a0f4a6926b835f73f956a6d8438e
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-04 10:44:31 -0300

    feat(dao): adicionar implementação JDBC de ProfessorDao

---
+++ src/model/dao/impl/ProfessorDaoJDBC.java
@@ -0,0 +1,45 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.ProfessorDao;
+import model.entities.Aluno;
+import model.entities.Disciplina;
+import model.entities.Professor;
+
+public class ProfessorDaoJDBC implements ProfessorDao {
+    @Override
+    public void insert(Aluno obj) {
+        
+    }
+
+    @Override
+    public void update(Aluno obj) {
+
+    }
+
+    @Override
+    public void deleteByID(Integer id) {
+
+    }
+
+    @Override
+    public void findById(Integer id) {
+
+    }
+
+    @Override
+    public void findByDisciplina(Disciplina obj) {
+
+    }
+
+    @Override
+    public void findByEspecialidade(String especialidade) {
+
+    }
+
+    @Override
+    public List<Professor> findAll() {
+        return List.of();
+    }
+}
