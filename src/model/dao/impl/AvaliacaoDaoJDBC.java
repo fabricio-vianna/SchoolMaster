00e9f3068b5f5e0e905345243e7e3fe34716a3a8
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-04 10:37:14 -0300

    feat(dao): adicionar implementação JDBC de AvaliacaoDao

---
+++ src/model/dao/impl/AvaliacaoDaoJDBC.java
@@ -0,0 +1,45 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.AvaliacaoDao;
+import model.entities.Aluno;
+import model.entities.Disciplina;
+import model.entities.Professor;
+
+public class AvaliacaoDaoJDBC implements AvaliacaoDao {
+    @Override
+    public void insert(Aluno obj) {
+
+    }
+
+    @Override
+    public void update(Aluno obj) {
+
+    }
+
+    @Override
+    public void deleteByID(Integer id) {
+
+    }
+
+    @Override
+    public void findById(Integer id) {
+
+    }
+
+    @Override
+    public void findByAluno(Aluno aluno) {
+
+    }
+
+    @Override
+    public void findByDisciplina(Disciplina disciplina) {
+
+    }
+
+    @Override
+    public List<Professor> findAll() {
+        return List.of();
+    }
+}
