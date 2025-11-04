b201c587c847a66f527e5bd5d662a9cab9876a03
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-04 10:41:40 -0300

    feat(dao): adicionar implementação JDBC de DisciplinaDao

---
+++ src/model/dao/impl/DisciplinaDaoJDBC.java
@@ -0,0 +1,45 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DisciplinaDao;
+import model.entities.Aluno;
+import model.entities.Curso;
+import model.entities.Professor;
+
+public class DisciplinaDaoJDBC implements DisciplinaDao {
+    @Override
+    public void insert(Aluno obj) {
+
+    }
+
+    @Override
+    public void update(Aluno obj) {
+
+    }
+
+    @Override
+    public void deleteByID(Integer id) {
+
+    }
+
+    @Override
+    public void findById(Integer id) {
+
+    }
+
+    @Override
+    public void findByCurso(Curso curso) {
+
+    }
+
+    @Override
+    public void findByProfessor(Professor professor) {
+
+    }
+
+    @Override
+    public List<Professor> findAll() {
+        return List.of();
+    }
+}
