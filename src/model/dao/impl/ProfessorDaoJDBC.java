b62b6ce5a8a8f833df6104d142f4aa7bab3aea36
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-11 10:56:15 -0300

    feat(dao): implementar método insert() em ProfessorDaoJDBC.java

--- src/model/dao/impl/ProfessorDaoJDBC.java
+++ src/model/dao/impl/ProfessorDaoJDBC.java
@@ -1,20 +1,94 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.ProfessorDao;
-import model.entities.Aluno;
 import model.entities.Disciplina;
 import model.entities.Professor;
 
 public class ProfessorDaoJDBC implements ProfessorDao {
-    @Override
-    public void insert(Aluno obj) {
-        
+
+    private Connection conn;
+
+    public ProfessorDaoJDBC(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
-    public void update(Aluno obj) {
+    public void insert(Professor obj) {
+        PreparedStatement stPessoa = null;
+        PreparedStatement stProfessor = null;
+        ResultSet rs = null;
+
+        try {
+            conn.setAutoCommit(false);
+
+            stPessoa = conn.prepareStatement(
+                    "INSERT INTO pessoa "
+                            + "(nome, cpf, email) "
+                            + "VALUES "
+                            + "(?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            stPessoa.setString(1, obj.getNome());
+            stPessoa.setString(2, obj.getCpf());
+            stPessoa.setString(3, obj.getEmail());
+
+            int rowsAffected = stPessoa.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Falha ao inserir pessoa, nenhuma linha afetada.");
+            }
+
+            rs = stPessoa.getGeneratedKeys();
+            int pessoaID;
+            if (rs.next()) {
+                pessoaID = rs.getInt(1);
+            } else {
+                throw new DbException("Falha ao inserir pessoa, nenhum ID obtido.");
+            }
+
+            stProfessor = conn.prepareStatement(
+                    "INSERT INTO professor "
+                            + "(id, especialidade) "
+                            + "VALUES "
+                            + "(?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            stProfessor.setInt(1, pessoaID);
+            stProfessor.setString(2, obj.getEspecialidade());
+
+            stProfessor.executeUpdate();
+
+            conn.commit();
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+                throw new DbException("Transação revertida! Erro: " + e.getMessage());
+            } catch (SQLException e1) {
+                throw new DbException("Erro ao tentar reverter transação! " + e1.getMessage());
+            }
+        } finally {
+            DB.closeStatement(stPessoa);
+            DB.closeStatement(stProfessor);
+            DB.closeResultSet(rs);
+            try {
+                conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void update(Professor obj) {
 
     }
 
