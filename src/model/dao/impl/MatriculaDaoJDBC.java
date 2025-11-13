ecb86d3ad687dcfdc72c9d69d1d6277773494e45
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-13 12:33:18 -0300

    feat(dao): implementar método insert() em MatriculaDaoJDBC.java

--- src/model/dao/impl/MatriculaDaoJDBC.java
+++ src/model/dao/impl/MatriculaDaoJDBC.java
@@ -1,20 +1,70 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.MatriculaDao;
 import model.entities.Aluno;
 import model.entities.Curso;
-import model.entities.Professor;
+import model.entities.Matricula;
 
 public class MatriculaDaoJDBC implements MatriculaDao {
-    @Override
-    public void insert(Aluno obj) {
 
+    private Connection conn;
+
+    public MatriculaDaoJDBC(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
-    public void update(Aluno obj) {
+    public void insert(Matricula obj) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO matricula "
+                            + "(id_aluno, id_curso, data_matricula, ativa) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setInt(1, obj.getAluno().getId());
+            st.setInt(2, obj.getCurso().getId());
+            st.setDate(3, Date.valueOf(obj.getDataMatricula()));
+            st.setBoolean(4, obj.isAtiva());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Falha ao inserir matricula, nenhuma linha afetada.");
+            }
+
+            rs = st.getGeneratedKeys();
+
+            if (rs.next()) {
+                int id = rs.getInt(1);
+            } else {
+                throw new DbException("Falha ao inserir matricula, nenhum ID obtido.");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException("Erro ao tentar reverter transação! " + e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public void update(Matricula obj) {
 
     }
 
@@ -24,18 +74,18 @@ public class MatriculaDaoJDBC implements MatriculaDao {
     }
 
     @Override
-    public void findById(Integer id) {
-
+    public Matricula findById(Integer id) {
+        return null;
     }
 
     @Override
-    public void findByAluno(Aluno aluno) {
-
+    public Matricula findByAluno(Aluno aluno) {
+        return null;
     }
 
     @Override
-    public void findByCurso(Curso curso) {
-
+    public Matricula findByCurso(Curso curso) {
+        return null;
     }
 
     @Override
@@ -44,7 +94,7 @@ public class MatriculaDaoJDBC implements MatriculaDao {
     }
 
     @Override
-    public List<Professor> findAll() {
+    public List<Matricula> findAll() {
         return List.of();
     }
 }
