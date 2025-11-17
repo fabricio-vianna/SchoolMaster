84a3bbe1cd3a6ac5a413206c75fe434a2ba82b80
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-17 13:51:12 -0300

    feat(dao): implementar método deleteById() em DisciplinaDaoJDBC.java

--- src/model/dao/impl/DisciplinaDaoJDBC.java
+++ src/model/dao/impl/DisciplinaDaoJDBC.java
@@ -68,7 +68,27 @@ public class DisciplinaDaoJDBC implements DisciplinaDao {
 
     @Override
     public void deleteByID(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "DELETE FROM disciplina "
+                            + "WHERE "
+                            + "id = ?");
+
+            st.setInt(1, id);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Id inválido ou já deletado");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException("Erro ao tentar deletar disciplina! " + e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
