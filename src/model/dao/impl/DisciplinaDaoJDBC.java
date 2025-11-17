2b48ccde97d3831a210daee356e2ec2267d9d7fc
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-17 10:41:45 -0300

    feat(dao): implementar método inert() em DisciplinaDaoJDBC.java

--- src/model/dao/impl/DisciplinaDaoJDBC.java
+++ src/model/dao/impl/DisciplinaDaoJDBC.java
@@ -1,20 +1,68 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DisciplinaDao;
-import model.entities.Aluno;
 import model.entities.Curso;
+import model.entities.Disciplina;
 import model.entities.Professor;
 
 public class DisciplinaDaoJDBC implements DisciplinaDao {
-    @Override
-    public void insert(Aluno obj) {
 
+    private Connection conn;
+
+    public DisciplinaDaoJDBC(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
-    public void update(Aluno obj) {
+    public void insert(Disciplina obj) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO disciplina " +
+                            "(nome, carga_horaria, id_professor, id_curso) " +
+                            "VALUES (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getNome());
+            st.setInt(2, obj.getCargaHoraria());
+            st.setInt(3, obj.getProfessor().getId());
+            st.setInt(4, obj.getCurso().getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Falha ao inserir disciplina, nenhuma linha afetada.");
+            }
+
+            rs = st.getGeneratedKeys();
+
+            if (rs.next()) {
+                int id = rs.getInt(1);
+            } else {
+                throw new DbException("Falha ao inserir disciplina, nenhum ID obtido.");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException("Erro ao tentar reverter transação! " + e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public void update(Disciplina obj) {
 
     }
 
@@ -24,18 +72,18 @@ public class DisciplinaDaoJDBC implements DisciplinaDao {
     }
 
     @Override
-    public void findById(Integer id) {
-
+    public Disciplina findById(Integer id) {
+        return null;
     }
 
     @Override
-    public void findByCurso(Curso curso) {
-
+    public Disciplina findByCurso(Curso curso) {
+        return null;
     }
 
     @Override
-    public void findByProfessor(Professor professor) {
-
+    public Disciplina findByProfessor(Professor professor) {
+        return null;
     }
 
     @Override
