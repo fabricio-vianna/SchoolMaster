00c7708d5a6d7292c599c70fc12c2842046bd590
fabricio-vianna <fab.vianna.1976@gmail.com>
2025-11-18 17:15:48 -0300

    feat(dao): implementar método insert() em AvaliacaoDaoJDBC.java

--- src/model/dao/impl/AvaliacaoDaoJDBC.java
+++ src/model/dao/impl/AvaliacaoDaoJDBC.java
@@ -1,20 +1,69 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.AvaliacaoDao;
-import model.entities.Aluno;
+import model.entities.Avaliacao;
 import model.entities.Disciplina;
 import model.entities.Professor;
 
 public class AvaliacaoDaoJDBC implements AvaliacaoDao {
-    @Override
-    public void insert(Aluno obj) {
 
+    private Connection conn;
+
+    public AvaliacaoDaoJDBC(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
-    public void update(Aluno obj) {
+    public void insert(Avaliacao obj) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO avaliacao "
+                            + "(id_aluno, id_disciplina, nota, frequencia) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setInt(1, obj.getAluno().getId());
+            st.setInt(2, obj.getDisciplina().getId());
+            st.setDouble(3, obj.getNota());
+            st.setInt(4, obj.getFrequencia());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0) {
+                throw new DbException("Falha ao inserir avaliação, nenhuma linha afetada.");
+            }
+
+            rs = st.getGeneratedKeys();
+
+            if (rs.next()) {
+                int id = rs.getInt(1);
+            } else {
+                throw new DbException("Falha ao inserir avaliação, nenhum ID obtido.");
+            }
+
+        } catch (SQLException e) {
+            throw new DbException("Erro ao tentar reverter transação! " + e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public void update(Avaliacao obj) {
 
     }
 
@@ -24,18 +73,18 @@ public class AvaliacaoDaoJDBC implements AvaliacaoDao {
     }
 
     @Override
-    public void findById(Integer id) {
-
+    public Avaliacao findById(Integer id) {
+        return null;
     }
 
     @Override
-    public void findByAluno(Aluno aluno) {
-
+    public Avaliacao findByAluno(Avaliacao aluno) {
+        return null;
     }
 
     @Override
-    public void findByDisciplina(Disciplina disciplina) {
-
+    public Avaliacao findByDisciplina(Disciplina disciplina) {
+        return null;
     }
 
     @Override
